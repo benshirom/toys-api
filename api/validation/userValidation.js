73bcb331570506495d0c1693e1c976ae001d34a5
benshirom <57137329+benshirom@users.noreply.github.com>
2022-10-11 15:11:36 +0300

    Add files via upload

--- api/validation/userValidation.js
+++ api/validation/userValidation.js
@@ -6,8 +6,8 @@ const Joi = require("joi");
 exports.validUser = (_reqBody) => {
     let joiSchema = Joi.object({
       name:Joi.string().min(2).max(99).required(),
-      email:Joi.string().min(2).max(99).email().required(),
-      password:Joi.string().min(3).max(99).required()
+      email:Joi.string().min(4).max(99).email().required(),
+      password:Joi.string().min(6).max(99).required()
     })
   
     return joiSchema.validate(_reqBody);
@@ -15,8 +15,8 @@ exports.validUser = (_reqBody) => {
   
   exports.validLogin = (_reqBody) => {
     let joiSchema = Joi.object({
-      email:Joi.string().min(2).max(99).email().required(),
-      password:Joi.string().min(3).max(99).required()
+      email:Joi.string().min(4).max(99).email().required(),
+      password:Joi.string().min(6).max(99).required()
     })
   
     return joiSchema.validate(_reqBody);
